63558a2c15be425a858b65d2d20092a4b12c40fd
luxon.jean.pierre@gmail.com <luxon.jean.pierre@gmail.com@494453de-2620-ff15-18dd-6fa4b6c61c45>
2014-08-24 22:24:16 +0000

    updated the LunatiX_engine reference

---
+++ reference/search/defines_2.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['lua_5fconfig_5ffile',['LUA_CONFIG_FILE',['../_l_x__config_8h.html#acb1ba22829e96be8f1c8fdd7aaf39d93',1,'LX_config.h']]],
+  ['luac_5fconfig_5ffile',['LUAC_CONFIG_FILE',['../_l_x__config_8h.html#a2ef5051105b6bddae56c268ab3ca5d0a',1,'LX_config.h']]],
+  ['lx_5faudio_5fdefault_5fchunksize',['LX_AUDIO_DEFAULT_CHUNKSIZE',['../_l_x__audio_8h.html#a6aa8726708d438425bc1dd4b3995d306',1,'LX_AUDIO_DEFAULT_CHUNKSIZE():&#160;LX_audio.h'],['../_l_x__lib_8h.html#a6aa8726708d438425bc1dd4b3995d306',1,'LX_AUDIO_DEFAULT_CHUNKSIZE():&#160;LX_lib.h']]],
+  ['lx_5faudio_5floop',['LX_AUDIO_LOOP',['../_l_x__audio_8h.html#af73565fa12573747ac75c2f20df76a7a',1,'LX_audio.h']]],
+  ['lx_5faudio_5fmono_5fsound',['LX_AUDIO_MONO_SOUND',['../_l_x__audio_8h.html#ae9109b43a41793e34b4afdc49444a457',1,'LX_AUDIO_MONO_SOUND():&#160;LX_audio.h'],['../_l_x__lib_8h.html#ae9109b43a41793e34b4afdc49444a457',1,'LX_AUDIO_MONO_SOUND():&#160;LX_lib.h']]],
+  ['lx_5faudio_5fnoloop',['LX_AUDIO_NOLOOP',['../_l_x__audio_8h.html#a348abaf17ae4f2fcea42aaaf094338fb',1,'LX_audio.h']]],
+  ['lx_5faudio_5fstereo_5fsound',['LX_AUDIO_STEREO_SOUND',['../_l_x__audio_8h.html#a313b1cb02464681409a0c7b3199c03b3',1,'LX_AUDIO_STEREO_SOUND():&#160;LX_audio.h'],['../_l_x__lib_8h.html#a313b1cb02464681409a0c7b3199c03b3',1,'LX_AUDIO_STEREO_SOUND():&#160;LX_lib.h']]]
+];
---
+++ reference/search/typedefs_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['lx_5faabb',['LX_AABB',['../_l_x___hitbox_8h.html#a37b8f71884cbc7980606891d577aba4e',1,'LX_Hitbox.h']]],
+  ['lx_5fcircle',['LX_Circle',['../_l_x___hitbox_8h.html#a0e4c0781acec05a8e8cb7897945ac06b',1,'LX_Hitbox.h']]],
+  ['lx_5fpoint',['LX_Point',['../_l_x___point_8h.html#a4d98e7f1555361272fa0839acfb954ee',1,'LX_Point.h']]]
+];
