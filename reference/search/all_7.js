d27602f7c97a896f6bdde0a1b27fc88e4fecc88b
luxon.jean.pierre@gmail.com <luxon.jean.pierre@gmail.com@494453de-2620-ff15-18dd-6fa4b6c61c45>
2015-01-21 09:55:38 +0000

    cleaned the branche content and updated the roadmap wiki

--- reference/search/all_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['radius',['radius',['../struct_l_x___circle.html#a4e47923c93432e1df66a0eff96810fed',1,'LX_Circle']]],
-  ['rect_5fsides',['RECT_SIDES',['../_l_x__physics_8h.html#aab4e29353858cb2060a5a0208a2280d4',1,'LX_physics.h']]]
-];
--- reference/search/variables_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['square_5fradius',['square_radius',['../struct_l_x___circle.html#a43b7905707ccf91271b1a4a95e90af9b',1,'LX_Circle']]],
-  ['str_5ferr',['str_err',['../class_l_x__configuration__exception.html#ad2489525e043db6e306fcc9a3594696a',1,'LX_configuration_exception::str_err()'],['../class_l_x___polygon___point__exception.html#a70d26505f8de1d1f2683c1dc4b8a6c93',1,'LX_Polygon_Point_exception::str_err()'],['../class_l_x___open_font__exception.html#a458ded9bfda123e1d0689d56f31ba904',1,'LX_OpenFont_exception::str_err()'],['../class_l_x__window__exception.html#ad121e4bc95a33113903befc2ba126183',1,'LX_window_exception::str_err()']]]
-];
