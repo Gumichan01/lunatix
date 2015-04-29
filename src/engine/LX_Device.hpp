882549264cb8c3ccd0447709a273086ce2c3a83a
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-04-30 00:29:18 +0200

    Added the structure of gamepad info and prototypes of functions

--- src/engine/LX_Device.hpp
+++ src/engine/LX_Device.hpp
@@ -16,7 +16,7 @@
 
 /**
 *	@file LX_Device.cpp
-*	@brief The LX_Device library
+*	@brief The LX_Device library. It gathers information about gamepad
 *	@author Luxon Jean-Pierre(Gumichan01)
 *	@version 0.3
 *
@@ -24,13 +24,32 @@
 
 #include <SDL2/SDL.h>
 
+
+#define LX_JOY_NAMESIZE 64
+
+
 namespace LX_Device{
+
+
+typedef struct LX_GamepadInfo{
+
+    SDL_JoystickID id;
+    char name[LX_JOY_NAMESIZE];
+    int numAxis;
+    int numBalls;
+    int numButtons;
+    int numHats;
+
+}LX_GamepadInfo;
 
 
     int numberOfDevices(void);
 
     const char * nameOf(SDL_Joystick * joy);
     const char * nameOf(SDL_GameController * controller);
+
+    int statGamepad(SDL_Joystick * joy, LX_GamepadInfo *info);
+    int statGamepad(SDL_GameController * gp, LX_GamepadInfo *info);
 
 };
 
