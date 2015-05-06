71a2621db07d54ca72a9918d50006ea6b16cd712
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-05-06 17:28:43 +0200

    Added a new test case

--- test/test-physics.cpp
+++ test/test-physics.cpp
@@ -1,5 +1,6 @@
 
 
+#include <cmath>
 
 #include <iostream>
 
@@ -10,7 +11,6 @@ using namespace LX_Physics;
 
 
 void test_euclide(void);
-void test_boxes(void);
 void test_collisionPointCircle(void);
 void test_collisionPointRect(void);
 void test_collision2Circle(void);
@@ -42,14 +42,14 @@ int main(int argc, char **argv)
 }
 
 
-
+// Test the euclidean functions
 void test_euclide(void)
 {
 
     LX_Point A = {1,1};
     LX_Point B = {10,1};
 
-    LX_Point C = {1,1};
+    LX_Point C = {0,0};
     LX_Point D = {10,10};
 
     int d;
@@ -57,9 +57,9 @@ void test_euclide(void)
     d = euclide_square_distance(&A,&B);
 
     if(d != 81)
-        cerr << "FAILURE - Bad square distance AB - expected : 81; got : " << d << endl;
+        cerr << "FAILURE - Bad square distance of AB - expected : 81; got : " << d << endl;
     else
-        cout << "SUCCESS - Good square distance AB : " << d << endl;
+        cout << "SUCCESS - Good square distance of AB : " << d << endl;
 
     d = euclide_distance(&A,&B);
 
@@ -68,9 +68,23 @@ void test_euclide(void)
     else
         cout << "SUCCESS - Good distance of AB : " << d << endl;
 
+    // Test CD
+
+    d = euclide_square_distance(&C,&D);
+
+    if(d != 200)
+        cerr << "FAILURE - Bad square distance of CD - expected : 200; got : " << d << endl;
+    else
+        cout << "SUCCESS - Good square distance of CD : " << d << endl;
+
+    d = euclide_distance(&C,&D);
+
+    if(d != ( (int) sqrt(200)) )
+        cerr << "FAILURE - Bad distance of CD - expected : " << (int) sqrt(200) << "; got : " << d << endl;
+    else
+        cout << "SUCCESS - Good distance of CD : " << d << endl;
 
 }
-
 
 
 
