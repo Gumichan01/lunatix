73c5e23024c7285863e4b9963e951a68fda2edbd
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-05-14 22:38:21 +0200

    Refactored some tests

--- test/test-renderer.cpp
+++ test/test-renderer.cpp
@@ -30,11 +30,12 @@ int main(int argc, char **argv)
         cout << "SUCCESS - LunatiX Engine have been initialized with success" << endl;
 
 
-    LX_Window win("Test window");
+    LX_Window win("Test window",LX_WINDOW_RENDERING);
     w = &win;
 
     test_window1(w);
     test_window2();
+    test_window3();
     test_rendering(w);
 
     cout << " ==== END Rendering ==== " << endl;
@@ -68,11 +69,6 @@ void test_window1(LX_Window *win)
         cout << "SUCCESS - the renderer is ready" << endl;
 
 
-    if(win->getSurface() == NULL)
-        cerr << "FAILURE - the surface was not initialized" << endl;
-    else
-        cout << "SUCCESS - the surface is ready" << endl;
-
     cout << " = END TEST = " << endl;
 
     SDL_Delay(100);
@@ -81,7 +77,7 @@ void test_window1(LX_Window *win)
 
 void test_window2(void)
 {
-    LX_Window win2("Hello",512,128,64,32,SDL_WINDOW_SHOWN);
+    LX_Window win2("Hello",512,128,64,32,LX_WINDOW_RENDERING,SDL_WINDOW_SHOWN);
 
     cout << " = TEST 2 window = " << endl;
 
@@ -96,12 +92,6 @@ void test_window2(void)
         cerr << "FAILURE - the renderer was not initialized" << endl;
     else
         cout << "SUCCESS - the renderer is ready" << endl;
-
-
-    if(win2.getSurface() == NULL)
-        cerr << "FAILURE - the surface was not initialized" << endl;
-    else
-        cout << "SUCCESS - the surface is ready" << endl;
 
 
     if(win2.getWidth() != 64)
@@ -124,7 +114,37 @@ void test_window2(void)
 
 void test_window3(void)
 {
+    LX_Window win3("Hello",512,128,64,32,LX_WINDOW_SURFACE,SDL_WINDOW_SHOWN);
 
+    cout << " = TEST 3 window = " << endl;
+
+    SDL_Delay(100);
+
+    if(win3.getWindow() == NULL)
+        cerr << "FAILURE - the window was not initialized" << endl;
+    else
+        cout << "SUCCESS - the window is ready" << endl;
+
+    if(win3.getSurface() == NULL)
+        cerr << "FAILURE - the surface was not initialized" << endl;
+    else
+        cout << "SUCCESS - the surface is ready" << endl;
+
+
+    if(win3.getWidth() != 64)
+        cerr << "FAILURE - width ; expected : 64; got: " << win3.getWidth() << endl;
+    else
+        cout << "SUCCESS - width 64" << endl;
+
+
+    if(win3.getHeight() != 32)
+        cerr << "FAILURE - height ; expected : 32; got: " << win3.getHeight() << endl;
+    else
+        cout << "SUCCESS - height 32" << endl;
+
+    cout << " = END TEST = " << endl;
+
+    SDL_Delay(100);
 }
 
 
@@ -135,6 +155,9 @@ void test_rendering(LX_Window *win)
 
     SDL_Texture *st = NULL;
     SDL_Rect pos = {100,100,150,120};
+
+
+    cout << " = TEST Rendering = " << endl;
 
     if(win == NULL)
     {
@@ -164,7 +187,22 @@ void test_rendering(LX_Window *win)
 
     win->clearRenderer();
 
+
+    if(win->putTextureAndRotate(st,NULL,&pos,-45) == false)
+        cerr << "FAILURE - failed to put the texture " << LX_GetError() << endl;
+    else
+        cout << "SUCCESS - Texture on the renderer with rotation" << endl;
+
+    win->updateRenderer();
+
+    SDL_Delay(2000);
+
+    win->clearRenderer();
+
+    SDL_Delay(2000);
     /// @todo test with rotation
+
+    cout << " = END TEST = " << endl;
 
 }
 
