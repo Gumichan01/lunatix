6530b2592c6ca115baa3dc297067e2cb31d479ce
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-05-22 22:47:35 +0200

    new file : Particle

---
+++ src/engine/Particle.hpp
@@ -0,0 +1,26 @@
+#ifndef PARTICLE_HPP_INCLUDED
+#define PARTICLE_HPP_INCLUDED
+
+
+
+/*
+*
+*	Copyright (C)  Luxon Jean-Pierre
+*	gumichan01.olympe.in
+*
+*
+*	Luxon Jean-Pierre (Gumichan01)
+*	luxon.jean.pierre@gmail.com
+*
+*/
+
+/**
+*	@file Particle.hpp
+*	@brief The Particle file
+*	@author Luxon Jean-Pierre(Gumichan01)
+*	@version 0.4
+*
+*
+*/
+
+#endif // PARTICLE_HPP_INCLUDED
