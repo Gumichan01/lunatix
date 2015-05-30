3bf68d8412913038367e29bf50c0a22766578b4d
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-05-30 14:24:11 +0200

    Added a new test case and and refactored the test file

--- test/test-file.cpp
+++ test/test-file.cpp
@@ -2,15 +2,24 @@
 
 
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 
 #include "../src/engine/Lunatix_engine.hpp"
 
+
+#define N 4
 
 using namespace std;
 using namespace LX_FileIO;
 
 
 void test_open(void);
+void test_read(void);
+void test_write(void);
+void test_tell(void);
+
+string str = "tmpFile";
 
 int main(int argc, char **argv)
 {
@@ -27,7 +36,10 @@ int main(int argc, char **argv)
 
 
     test_open();
+    test_write();
+    test_read();
 
+    //remove(str.c_str());
 
     cout << " ==== End File ==== " << endl;
 }
@@ -42,7 +54,7 @@ void test_open(void)
 
     string str1 = "data/explosion.png";
     char * null = NULL;
-    string str3 = "data/invalid_file";
+    string str3 = "invalid_file";
 
     cout << " = TEST open = " << endl;
 
@@ -69,7 +81,7 @@ void test_open(void)
 
         invalidString = new LX_File(null,LX_FILEIO_RDONLY);
 
-        cout << "FAILURE - NULL was loaded (o_o); Expected : IOexception; got : a valid reference " << endl;
+        cerr << "FAILURE - NULL was loaded (o_o); Expected : IOexception; got : a valid reference " << endl;
 
     }catch(IOException &ex)
     {
@@ -81,7 +93,7 @@ void test_open(void)
 
         f1 = new LX_File(str1.c_str(),0x00000000);
 
-        cout << "FAILURE - NULL was loaded (o_o); Expected : IOexception; got : a valid reference " << endl;
+        cerr << "FAILURE - mode was not set (o_o); Expected : IOexception; got : a valid reference " << endl;
 
     }catch(IOException &exe)
     {
@@ -93,7 +105,7 @@ void test_open(void)
 
         notExistFile = new LX_File(str3.c_str(),LX_FILEIO_RDONLY);
 
-        cout << "FAILURE - an invalid file was loaded (o_o); Expected : IOexception; got : a valid reference " << endl;
+        cout << "FAILURE - An invalid file was loaded (o_o); Expected : IOexception; got : a valid reference " << endl;
 
     }catch(IOException &ioe)
     {
@@ -104,10 +116,60 @@ void test_open(void)
 
 }
 
+void test_read(void)
+{
+    cout << " = TEST read = " << endl;
+
+    int lus = 0;
+    char buf[N + 1];
+    LX_File f(str.c_str(),LX_FILEIO_RDONLY);
+
+    lus = f.read(buf,sizeof(char),N);
+
+    if(lus == -1)
+        cerr << "FAILURE - Expected : a positive value or zero; got : -1 " << endl;
+    else
+    {
+        cout << "SUCCESS - Received " << lus << " bytes from " << str << endl;
+        buf[lus] = 0;
+        cout << "INFO - data received : " << buf << endl;
+    }
+
+    f.close();
+
+    cout << " = END TEST = " << endl;
+}
 
 
+void test_write(void)
+{
+    cout << " = TEST write = " << endl;
+
+    int lus = 0;
+    char buf[N];
+    LX_File f(str.c_str(),LX_FILEIO_WRONLY);
+
+    strncpy(buf,"GUMI",N);
+
+    lus = f.write(buf,sizeof(char),N);
+
+    if(lus == -1)
+        cerr << "FAILURE - Expected : a positive value or zero; got : -1 " << endl;
+    else
+        cout << "SUCCESS - Wrote " << lus << " bytes on " << str << endl;
 
 
+    lus = f.write("CHAN01");
+
+    if(lus == -1)
+        cerr << "FAILURE - Expected : a positive value or zero; got : -1 " << endl;
+    else
+        cout << "SUCCESS - Wrote " << lus << " bytes on " << str << endl;
+
+    f.close();
+
+    cout << " = END TEST = " << endl;
+}
 
 
 
