e093c3d671ab6d89905e94b7eda8721f37c5f0a1
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-06-29 14:52:41 +0200

    Integrated the gamepad into the engine

--- src/engine/Lunatix_engine.hpp
+++ src/engine/Lunatix_engine.hpp
@@ -39,7 +39,7 @@
 #include "LX_WindowManager.hpp"
 #include "LX_FileIO.hpp"
 #include "LX_FileBuffer.hpp"
-#include "LX_Device.hpp"
+#include "LX_Gamepad.hpp"
 #include "LX_SystemInfo.hpp"
 #include "LX_Random.hpp"
 #include "LX_MessageBox.hpp"
