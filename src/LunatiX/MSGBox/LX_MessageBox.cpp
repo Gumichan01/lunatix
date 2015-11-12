1252f78cc8e0fd89435843dbe9c11b8b8f248643
Gumichan01 <luxon.jean.pierre@gmail.com>
2015-11-12 22:08:10 +0100

    fixed a typo

--- src/LunatiX/MSGBox/LX_MessageBox.cpp
+++ src/LunatiX/MSGBox/LX_MessageBox.cpp
@@ -36,7 +36,7 @@ namespace LX_MSGBox
 *           - LX_MSG_INFO : Information message
 *   @param title The title of the box, UTF-8 format
 *   @param msg The text to display in the box, UTF-8 format
-*   @param w The parent window, or NULL for no parent
+*   @param w The parent window, or a null pointer for no parent
 *
 *   @return TRUE on success, FALSE on error
 */
