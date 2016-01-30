055ad8e803390b0a1b242bececa47eabc620d2cf
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-01-30 13:08:25 +0100

    Fixed a typo

--- include/LunatiX/LX_Haptic.hpp
+++ include/LunatiX/LX_Haptic.hpp
@@ -15,7 +15,7 @@
 
 /**
 *	@file LX_Haptic.hpp
-*	@brief The library that deals force feedbaxck
+*	@brief The library that deals force feedback
 *	@author Luxon Jean-Pierre(Gumichan01)
 *	@version 0.7
 *
