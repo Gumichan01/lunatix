e56ccb78f6fe65902c33ec347cc286a4e135e266
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-06-20 16:53:26 +0200

    LX_Text: cursor browsing into the utf8 string.
     - A new character can be integrated in an existing string.
     - New debug logs have been added.
    
    --

--- src/LunatiX/Text/LX_Text.cpp
+++ src/LunatiX/Text/LX_Text.cpp
@@ -86,15 +86,26 @@ void LX_TextInput::eventLoop(LX_RedrawCallback& redraw)
 // Handle the keyboard input
 void LX_TextInput::keyboardInput(SDL_Event& ev)
 {
+    const size_t oldcursor = cursor;
+
     switch(ev.key.keysym.sym)
     {
         case SDLK_ESCAPE:       done = true;
                                 break;
 
         case SDLK_BACKSPACE:    utf8Pop();
+                                if(cursor > 0) {cursor -= 1;}
                                 break;
 
+        case SDLK_LEFT:         if(cursor > 0) {cursor -= 1;}
+                                break;
+
+        case SDLK_RIGHT:        if(cursor < u8text.utf8_length()) {cursor += 1;}
+                                break;
     }
+
+    if(oldcursor != cursor)
+        LX_Log::log("Input - cursor at %d",cursor);
 }
 
 
@@ -109,7 +120,22 @@ void LX_TextInput::textInput(SDL_Event& ev)
 
     try
     {
-        u8text += ev.text.text;
+        const size_t u8len = u8text.utf8_length();
+
+        if(cursor == u8len)
+        {
+            u8text += ev.text.text;
+        }
+        else
+        {
+            UTF8string ntext(ev.text.text);
+            UTF8string rtmp = u8text.utf8_substr(cursor);
+            UTF8string ltmp = u8text.utf8_substr(0,cursor);
+            u8text = ltmp + ntext + rtmp;
+        }
+
+        cursor += 1;
+        LX_Log::log("Input - cursor at %d",cursor);
     }
     catch(...)
     {
