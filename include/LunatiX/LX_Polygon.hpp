b18ca09adcea6f3b47c3a0dab1993640a45c6b80
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-07-14 12:59:58 +0200

    Refactored the documentation of LX_Polygon

--- include/LunatiX/LX_Polygon.hpp
+++ include/LunatiX/LX_Polygon.hpp
@@ -35,19 +35,24 @@ struct LX_Vector2D;
 *   @brief The exception class of LX_Window
 *
 *   This class describes the exception occured when
-*   the SDL_Window instance cannot be loaded.
+*   the polygon cannot be loaded.
 */
 class LX_PolygonException : public std::exception
 {
-    std::string stringError;
+    std::string string_error;
 
 public :
 
+    /// Construct the exception
     LX_PolygonException(std::string err);
+
+    /// Construct the exception by copy
     LX_PolygonException(const LX_PolygonException& pex);
 
+    /// Get the error message
     const char * what() const noexcept;
 
+    /// Destroy the exception
     ~LX_PolygonException() noexcept;
 };
 
@@ -73,21 +78,133 @@ class LX_Polygon
 
 public :
 
+    /**
+    *   @fn LX_Polygon::LX_Polygon(const unsigned int nb)
+    *
+    *   Create a new instance of LX_Polygon
+    *
+    *   @param nb The number of edges to build the polygon
+    *
+    */
     LX_Polygon(const unsigned int nb);
 
+    /**
+    *   @fn bool LX_Polygon::addPoint(const int x, const int y)
+    *
+    *   Set a new point into the polygon according to its coordinates
+    *
+    *   @param x The x position of the point
+    *   @param y The y position of the point
+    *
+    *   @return TRUE if the point was added, FALSE otherwise
+    *
+    */
     bool addPoint(const int x, const int y);
+
+    /**
+    *   @fn bool LX_Polygon::addPoint(LX_Point *p)
+    *
+    *   Set a new point into the polygon
+    *
+    *   @param p The new edge to add
+    *
+    *   @return TRUE if the point was added, FALSE otherwise
+    *
+    */
     bool addPoint(LX_Point *p);
 
+    /**
+    *   @fn unsigned int LX_Polygon::numberOfEdges(void) const
+    *
+    *   Get the maximum number of edges
+    *
+    *   @return The maximum number of edges of the polygon
+    *
+    *   @note   The number of edges is the same as the defined value
+    *           in the polygon constructor
+    *
+    */
     unsigned int numberOfEdges(void) const;
+
+    /**
+    *   @fn unsigned int LX_Polygon::numberOfRealEdges(void) const
+    *
+    *   Get the real number of defined edges
+    *
+    *   @return The real number of edges of the polygon
+    *
+    *   @note   The number of edges may be different from the defined value
+    *           in the polygon constructor, because the points of the polygon
+    *           are not completely set.
+    *
+    */
     unsigned int numberOfRealEdges(void) const;
 
+    /**
+    *   @fn LX_Point LX_Polygon::getPoint(const unsigned int index) const
+    *
+    *   Get the point at a specified position
+    *
+    *   @param index The index of the point
+    *
+    *   @return A copy of the point
+    *
+    *   @exception  LX_PolygonException If the index refers
+    *               to an out of bounds position
+    *
+    */
     LX_Point getPoint(const unsigned int index) const;
 
-    void move(const float vx, const float vy);
-    void move(const LX_Vector2D& v);
-    void moveTo(int vx, int vy);
+    /**
+    *   @fn bool LX_Polygon::isConvex(void)
+    *
+    *   Check the convexity of the polygon
+    *
+    *   @return TRUE if the polygon is convex, false otherwise
+    *
+    *   @note Actually, the convexity of the polygon is dynamically evaluated
+    *           each time a new point in added using LX_Polygon::addPoint().
+    *           The result is stored in an internal variable
+    *
+    */
     bool isConvex(void);
 
+    /**
+    *   @fn void LX_Polygon::move(const float vx, const float vy)
+    *
+    *   Move the polygon to a direction
+    *
+    *   @param vx The x direction
+    *   @param vy The y direction
+    */
+    void move(const float vx, const float vy);
+
+    /**
+    *   @fn void LX_Polygon::move(const LX_Vector2D& v)
+    *
+    *   Move the polygon to a direction
+    *
+    *   @param v The vector that indicates the direction
+    */
+    void move(const LX_Vector2D& v);
+
+    /**
+    *   @fn void LX_Polygon::moveTo(int vx, int vy)
+    *
+    *   Move the polygon to a position
+    *
+    *   @param vx The x position
+    *   @param vy The y position
+    *
+    */
+    void moveTo(int vx, int vy);
+
+    /**
+    *   @fn LX_Polygon::~LX_Polygon()
+    *
+    *   Destroy the instance of the polygon
+    *
+    */
     ~LX_Polygon();
 };
 
--- src/LunatiX/Physics/LX_Polygon.cpp
+++ src/LunatiX/Physics/LX_Polygon.cpp
@@ -29,51 +29,19 @@ using namespace std;
 namespace LX_Physics
 {
 
-/**
-*   @fn LX_PolygonException::LX_PolygonException(std::string err)
-*
-*   Build the LX_PolygonException class
-*   @param err The error string
-*
-*/
-LX_PolygonException::LX_PolygonException(std::string err)
-{
-    stringError = err;
-}
+LX_PolygonException::LX_PolygonException(std::string err) : string_error(err) {}
 
-/**
-*   @fn LX_PolygonException::LX_PolygonException(const LX_PolygonException& pex)
-*   @param pex Exception
-*/
 LX_PolygonException::LX_PolygonException(const LX_PolygonException& pex)
-{
-    stringError = pex.stringError;
-}
+    : string_error(pex.string_error) {}
 
-
-/**
-*   @fn const char * LX_PolygonException::what() const noexcept
-*
-*   Get the error string
-*
-*   @return The error string
-*/
 const char * LX_PolygonException::what() const noexcept
 {
-    return stringError.c_str();
+    return string_error.c_str();
 }
 
 LX_PolygonException::~LX_PolygonException() noexcept {}
 
 
-/**
-*   @fn LX_Polygon::LX_Polygon(const unsigned int nb)
-*
-*   Create a new instance of LX_Polygon
-*
-*   @param nb The number of edges to build the polygon
-*
-*/
 LX_Polygon::LX_Polygon(const unsigned int nb)
     : points(nullptr),nbPoints(nb), cursor(0), convex(false)
 {
@@ -90,18 +58,7 @@ LX_Polygon::LX_Polygon(const unsigned int nb)
 }
 
 
-/**
-*   @fn bool LX_Polygon::addPoint(const int x, const int y)
-*
-*   Set a new point into the polygon according to its coordinates
-*
-*   @param x The x position of the point
-*   @param y The y position of the point
-*
-*   @return TRUE if the point was added, FALSE otherwise
-*
-*/
-bool LX_Polygon::addPoint(const int x, const int y)
+bool LX_Polygon::addPoint(const int x, const int y) /// @todo refactor addPoint
 {
     if(cursor < nbPoints)
     {
@@ -119,70 +76,24 @@ bool LX_Polygon::addPoint(const int x, const int y)
 }
 
 
-/**
-*   @fn bool LX_Polygon::addPoint(LX_Point *p)
-*
-*   Set a new point into the polygon
-*
-*   @param p The new edge to add
-*
-*   @return TRUE if the point was added, FALSE otherwise
-*
-*/
 bool LX_Polygon::addPoint(LX_Point *p)
 {
     return addPoint(p->x,p->y);
 }
 
 
-/**
-*   @fn unsigned int LX_Polygon::numberOfEdges(void) const
-*
-*   Get the maximum number of edges
-*
-*   @return The maximum number of edges of the polygon
-*
-*   @note   The number of edges is the same as the defined value
-*           in the polygon constructor
-*
-*/
 unsigned int LX_Polygon::numberOfEdges(void) const
 {
     return nbPoints;
 }
 
 
-/**
-*   @fn unsigned int LX_Polygon::numberOfRealEdges(void) const
-*
-*   Get the real number of defined edges
-*
-*   @return The real number of edges of the polygon
-*
-*   @note   The number of edges may be different from the defined value
-*           in the polygon constructor, because the points of the polygon
-*           are not completely set.
-*
-*/
 unsigned int LX_Polygon::numberOfRealEdges(void) const
 {
     return cursor;
 }
 
 
-/**
-*   @fn LX_Point LX_Polygon::getPoint(const unsigned int index) const
-*
-*   Get the point at a specified position
-*
-*   @param index The index of the point
-*
-*   @return A copy of the point
-*
-*   @exception  LX_PolygonException If the index refers
-*               to an out of bounds position
-*
-*/
 LX_Point LX_Polygon::getPoint(const unsigned int index) const
 {
     if(index < cursor)
@@ -192,29 +103,13 @@ LX_Point LX_Polygon::getPoint(const unsigned int index) const
 }
 
 
-/**
-*   @fn bool LX_Polygon::isConvex(void)
-*
-*   Check the convexity of the polygon
-*
-*   @return TRUE if the polygon is convex, false otherwise
-*
-*   @note Actually, the convexity of the polygon is dynamically evaluated
-*           each time a new point in added using LX_Polygon::addPoint().
-*           The result is stored in an internal variable
-*
-*/
 bool LX_Polygon::isConvex(void)
 {
     return convex;
 }
 
 
-/*
-*
-*   Evaluate the convexity of the polygon
-*
-*/
+// Evaluate the convexity of the polygon
 void LX_Polygon::convexity(void)
 {
     // Vectors
@@ -296,15 +191,7 @@ void LX_Polygon::convexity(void)
 }
 
 
-/**
-*   @fn void LX_Polygon::move(const float vx, const float vy)
-*
-*   Move the polygon to a direction
-*
-*   @param vx The x direction
-*   @param vy The y direction
-*
-*/
+
 void LX_Polygon::move(const float vx, const float vy)
 {
     const LX_Vector2D vel(vx,vy);
@@ -321,29 +208,12 @@ void LX_Polygon::move(const float vx, const float vy)
 }
 
 
-/**
-*   @fn void LX_Polygon::move(const LX_Vector2D& v)
-*
-*   Move the polygon to a direction
-*
-*   @param v The vector that indicates the direction
-*
-*/
 void LX_Polygon::move(const LX_Vector2D& v)
 {
     move(v.vx, v.vy);
 }
 
 
-/**
-*   @fn void LX_Polygon::moveTo(int vx, int vy)
-*
-*   Move the polygon to a position
-*
-*   @param vx The x position
-*   @param vy The y position
-*
-*/
 void LX_Polygon::moveTo(int vx, int vy)
 {
     movePointTo(points[0],vx,vy);
@@ -359,12 +229,6 @@ void LX_Polygon::moveTo(int vx, int vy)
 }
 
 
-/**
-*   @fn LX_Polygon::~LX_Polygon()
-*
-*   Destroy the instance of the polygon
-*
-*/
 LX_Polygon::~LX_Polygon()
 {
     delete [] points;
