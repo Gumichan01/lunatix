eda54c820774df8bae6040b5a3574e7007e95cdc
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-07-14 17:57:45 +0200

    Refactoring of LX_FileIO : documentation

--- include/LunatiX/LX_FileIO.hpp
+++ include/LunatiX/LX_FileIO.hpp
@@ -21,7 +21,7 @@
 *
 */
 
-#include <string>
+#include <LunatiX/utils/utf8_string.hpp>
 
 #include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_rwops.h>
@@ -58,14 +58,19 @@ namespace LX_FileIO
 */
 class IOException: public std::exception
 {
-    std::string stringError;
+    std::string string_error;
 
 public :
 
+    /// Construct the Input/Output exception
     IOException(std::string err);
+    /// Construct the Input/Output exception by copy
     IOException(const IOException& io);
 
+    /// Get the error message
     const char * what() const noexcept;
+
+    /// Destruction
     ~IOException() noexcept;
 };
 
@@ -86,22 +91,161 @@ class LX_File
 
 public :
 
+    /**
+    *   @fn LX_File(std::string filename, const Uint32 mode)
+    *
+    *   Open the file given in argument according to the mode requested
+    *
+    *   @param filename The file to open
+    *   @param mode The mode to be used for opening the file.
+    *               It is one of these following :
+    *               - ::LX_FILEIO_RDONLY
+    *               - ::LX_FILEIO_WRONLY
+    *               - ::LX_FILEIO_APPEND
+    *               - ::LX_FILEIO_RDWR
+    *               - ::LX_FILEIO_RDAP
+    *               - ::LX_FILEIO_WRTR
+    *
+    *   @exception IOException If one of these aruguments are invalid
+    *               or the file is not openable
+    *
+    */
     LX_File(std::string filename, const Uint32 mode);
 
+    /**
+    *   @fn size_t read(void *ptr,size_t data_size,size_t max_num)
+    *
+    *   Read the file
+    *
+    *   @param ptr The pointer to a buffer to read data into
+    *   @param data_size The size of each object to read, in bytes
+    *   @param max_num The maximum number of objects to read
+    *
+    *   @return The number of objects read. 0 at error or end of file
+    *
+    *   @note It can read less bytes than expected.
+    */
     size_t read(void *ptr,size_t data_size,size_t max_num);
+
+    /**
+    *   @fn size_t readExactly(void *ptr,size_t data_size,size_t num)
+    *
+    *   Read exactly max_num bytes of the file
+    *
+    *   @param ptr The pointer to a buffer to read data into
+    *   @param data_size The size of each object to read, in bytes
+    *   @param num The maximum number of objects to read
+    *
+    *   @return The number of objects read. 0 at error or end of file
+    *
+    *   @note   This function assures that exactly num bytes will be read
+    *
+    */
     size_t readExactly(void *ptr,size_t data_size,size_t num);
+
+    /**
+    *   @fn size_t write(void *ptr,size_t data_size,size_t num)
+    *
+    *   Write on the file
+    *
+    *   @param ptr The pointer to a buffer containing data to write
+    *   @param data_size The size of an object to write, in bytes
+    *   @param num The maximum number of objects to write
+    *
+    *   @return The number of objects written.
+    *           This value will be less than num on error
+    *
+    */
     size_t write(void *ptr,size_t data_size,size_t num);
+
+    /**
+    *   @fn size_t write(std::string str)
+    *
+    *   Write a string on the file
+    *
+    *   @param str The string to write
+    *
+    *   @return The number of characters written.
+    *           This value will be less than the string length on error
+    *
+    */
     size_t write(std::string str);
 
+    /**
+    *   @fn Sint64 seek(Sint64 offset, int whence)
+    *
+    *   Seek for a position the file
+    *
+    *   @param offset An offset in bytes, relative to the whence; can be negative
+    *   @param whence Any of LX_SEEK_SET, LX_SEEK_CUR and LX_SEEK_END
+    *
+    *   @return The final offset in the data stream. -1 on error
+    *
+    */
     Sint64 seek(Sint64 offset, int whence);
+
+    /**
+    *   @fn Sint64 tell(void)
+    *
+    *   Get the position in a file
+    *
+    *   @return The current offset of the stream.
+    *           -1 if the position cannot be determined
+    *
+    */
     Sint64 tell(void);
+
+    /**
+    *   @fn Sint64 size(void)
+    *
+    *   Get the size of a file
+    *
+    *   @return The size of the file.
+    *           -1 on failure
+    *
+    */
     Sint64 size(void);
 
+    /**
+    *   @fn void close(void)
+    *
+    *   Close the file
+    *
+    */
     SDL_Surface * getSurfaceFromData(void);
+
+    /**
+    *   @fn SDL_Surface * getSurfaceFromData(void)
+    *
+    *   Try to load a surface from the file
+    *
+    *   @return A valid pointer to a surface if the file is an image file.
+    *           a null pointer otherwise
+    *
+    *   @note This function needs to read the entire file to create the surface.
+    *           So the cursor is reinitialized to 0 to do that.
+    *           However, It saves the old position of the cursor and restores it
+    *           at the end of the call;
+    *
+    */
     const char * getFilename(void);
 
+    /**
+    *   @fn const char * getFilename(void)
+    *
+    *   Get the name of the file the instance refers to
+    *
+    *   @return The name of the file
+    *
+    */
     void close(void);
 
+    /**
+    *   @fn ~LX_File()
+    *
+    *   Destroy the instance closing the file
+    *
+    */
     ~LX_File();
 };
 
--- src/LunatiX/FileIO/LX_FileIO.cpp
+++ src/LunatiX/FileIO/LX_FileIO.cpp
@@ -17,7 +17,6 @@
 *
 */
 
-
 #include <SDL2/SDL_image.h>
 
 #include <LunatiX/LX_FileIO.hpp>
@@ -27,62 +26,18 @@
 namespace LX_FileIO
 {
 
-/**
-*   @fn IOException::IOException(std::string err)
-*   @param err The error string
-*/
-IOException::IOException(std::string err)
-{
-    stringError = err;
-}
+IOException::IOException(std::string err) : string_error(err) {}
 
-/**
-*   @fn IOException::IOException(const IOException& io)
-*   @param io The exception
-*/
-IOException::IOException(const IOException& io)
-{
-    stringError = io.stringError;
-}
+IOException::IOException(const IOException& io) : string_error(io.string_error) {}
 
-
-
-/**
-*   @fn const char * IOException::what() const noexcept
-*
-*   Get the error string
-*
-*   @return The error string
-*
-*/
 const char * IOException::what() const noexcept
 {
-    return stringError.c_str();
+    return string_error.c_str();
 }
-
 
 IOException::~IOException() noexcept {}
 
 
-/**
-*   @fn LX_File::LX_File(std::string filename, const Uint32 mode)
-*
-*   The constuctor
-*
-*   @param filename The file to open
-*   @param mode The mode to be used for opening the file.
-*               It is one of these following :
-*               - ::LX_FILEIO_RDONLY
-*               - ::LX_FILEIO_WRONLY
-*               - ::LX_FILEIO_APPEND
-*               - ::LX_FILEIO_RDWR
-*               - ::LX_FILEIO_RDAP
-*               - ::LX_FILEIO_WRTR
-*
-*   @exception IOException If one of these aruguments are invalid
-*               or the file is not openable
-*
-*/
 LX_File::LX_File(std::string filename, const Uint32 mode)
     : name(filename), data(nullptr)
 {
@@ -93,14 +48,6 @@ LX_File::LX_File(std::string filename, const Uint32 mode)
 }
 
 
-/**
-*   @fn void LX_File::open(const Uint32 mode)
-*
-*   This private function opens the file
-*
-*   @param mode The mode to be used for opening the file
-*
-*/
 void LX_File::open(const Uint32 mode)
 {
     std::string str = "LX_File : ";
@@ -135,39 +82,12 @@ void LX_File::open(const Uint32 mode)
 }
 
 
-/**
-*   @fn size_t LX_File::read(void *ptr,size_t data_size,size_t max_num)
-*
-*   Read the file
-*
-*   @param ptr The pointer to a buffer to read data into
-*   @param data_size The size of each object to read, in bytes
-*   @param max_num The maximum number of objects to read
-*
-*   @return The number of objects read. 0 at error or end of file
-*
-*   @note It can read less bytes than expected.
-*/
 size_t LX_File::read(void *ptr,size_t data_size,size_t max_num)
 {
     return SDL_RWread(data,ptr,data_size,max_num);
 }
 
 
-/**
-*   @fn size_t LX_File::readExactly(void *ptr,size_t data_size,size_t num)
-*
-*   Read exactly max_num bytes of the file
-*
-*   @param ptr The pointer to a buffer to read data into
-*   @param data_size The size of each object to read, in bytes
-*   @param num The maximum number of objects to read
-*
-*   @return The number of objects read. 0 at error or end of file
-*
-*   @note   This function assures that exactly num bytes will be read
-*
-*/
 size_t LX_File::readExactly(void *ptr,size_t data_size,size_t num)
 {
     size_t total_read = 0;
@@ -196,36 +116,12 @@ size_t LX_File::readExactly(void *ptr,size_t data_size,size_t num)
 }
 
 
-/**
-*   @fn size_t LX_File::write(void *ptr,size_t data_size,size_t num)
-*
-*   Write on the file
-*
-*   @param ptr The pointer to a buffer containing data to write
-*   @param data_size The size of an object to write, in bytes
-*   @param num The maximum number of objects to write
-*
-*   @return The number of objects written.
-*           This value will be less than num on error
-*
-*/
 size_t LX_File::write(void *ptr,size_t data_size,size_t num)
 {
     return SDL_RWwrite(data,ptr,data_size,num);
 }
 
 
-/**
-*   @fn size_t LX_File::write(std::string str)
-*
-*   Write a string on the file
-*
-*   @param str The string to write
-*
-*   @return The number of characters written.
-*           This value will be less than the string length on error
-*
-*/
 size_t LX_File::write(std::string str)
 {
     size_t len = str.length();
@@ -233,59 +129,24 @@ size_t LX_File::write(std::string str)
 }
 
 
-/**
-*   @fn Sint64 LX_File::seek(Sint64 offset, int whence)
-*
-*   Seek for a position the file
-*
-*   @param offset An offset in bytes, relative to the whence; can be negative
-*   @param whence Any of LX_SEEK_SET, LX_SEEK_CUR and LX_SEEK_END
-*
-*   @return The final offset in the data stream. -1 on error
-*
-*/
 Sint64 LX_File::seek(Sint64 offset, int whence)
 {
     return SDL_RWseek(data,offset,whence);
 }
 
 
-/**
-*   @fn Sint64 LX_File::tell(void)
-*
-*   Get the position in a file
-*
-*   @return The current offset of the stream.
-*           -1 if the position cannot be determined
-*
-*/
 Sint64 LX_File::tell(void)
 {
     return SDL_RWtell(data);
 }
 
 
-/**
-*   @fn Sint64 LX_File::size(void)
-*
-*   Get the size of a file
-*
-*   @return The size of the file.
-*           -1 on failure
-*
-*/
 Sint64 LX_File::size(void)
 {
     return SDL_RWsize(data);
 }
 
 
-/**
-*   @fn void LX_File::close(void)
-*
-*   Close the file
-*
-*/
 void LX_File::close(void)
 {
     if(data != nullptr)
@@ -296,20 +157,6 @@ void LX_File::close(void)
 }
 
 
-/**
-*   @fn SDL_Surface * LX_File::getSurfaceFromData(void)
-*
-*   Try to load a surface from the file
-*
-*   @return A valid pointer to a surface if the file is an image file.
-*           a null pointer otherwise
-*
-*   @note This function needs to read the entire file to create the surface.
-*           So the cursor is reinitialized to 0 to do that.
-*           However, It saves the old position of the cursor and restores it
-*           at the end of the call;
-*
-*/
 SDL_Surface * LX_File::getSurfaceFromData(void)
 {
     SDL_Surface * surface = nullptr;
@@ -322,25 +169,13 @@ SDL_Surface * LX_File::getSurfaceFromData(void)
     return surface;
 }
 
-/**
-*   @fn const char * LX_File::getFilename(void)
-*
-*   Get the name of the file the instance refers to
-*
-*   @return The name of the file
-*
-*/
+
 const char * LX_File::getFilename(void)
 {
     return name.c_str();
 }
 
-/**
-*   @fn LX_File::~LX_File()
-*
-*   Destroy the instance closing the file
-*
-*/
+
 LX_File::~LX_File()
 {
     close();
