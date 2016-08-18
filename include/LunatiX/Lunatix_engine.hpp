0da7a5abd26d766259be60822d98310109b46649
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-08-18 13:40:13 +0200

    Refactored the library

--- include/LunatiX/Lunatix_engine.hpp
+++ include/LunatiX/Lunatix_engine.hpp
@@ -31,35 +31,50 @@
 #error "Not supported system"
 #endif
 
+// Initialization
 #include "LX_Library.hpp"
 #include "LX_Config.hpp"
-#include "LX_Device.hpp"
-#include "LX_Haptic.hpp"
+#include "LX_Version.hpp"
+
+// Graphics
 #include "LX_Image.hpp"
 #include "LX_OpenGL.hpp"
 #include "LX_TrueTypeFont.hpp"
-#include "LX_Mixer.hpp"
-#include "LX_Chunk.hpp"
-#include "LX_Music.hpp"
-#include "LX_Physics.hpp"
-#include "LX_Polygon.hpp"
-#include "LX_Particle.hpp"
-#include "LX_ParticleSystem.hpp"
 #include "LX_Window.hpp"
 #include "LX_WindowManager.hpp"
+
+// Audio
+#include "LX_Chunk.hpp"
+#include "LX_Mixer.hpp"
+#include "LX_Music.hpp"
+
+// Game controller
+#include "LX_Device.hpp"
+#include "LX_Gamepad.hpp"
+#include "LX_Haptic.hpp"
+
+// Physics
+#include "LX_Particle.hpp"
+#include "LX_ParticleSystem.hpp"
+#include "LX_Physics.hpp"
+#include "LX_Polygon.hpp"
+
+// System
+#include "LX_Channel.hpp"
+#include "LX_Error.hpp"
 #include "LX_FileIO.hpp"
 #include "LX_FileBuffer.hpp"
-#include "LX_Gamepad.hpp"
-#include "LX_SystemInfo.hpp"
-#include "LX_Random.hpp"
-#include "LX_MessageBox.hpp"
-#include "LX_Error.hpp"
-#include "LX_Version.hpp"
 #include "LX_Log.hpp"
-#include "LX_Text.hpp"
-#include "LX_Thread.hpp"
+#include "LX_MessageBox.hpp"
+#include "LX_Random.hpp"
 #include "LX_Sync.hpp"
-#include "LX_Channel.hpp"
+#include "LX_SystemInfo.hpp"
+#include "LX_Thread.hpp"
+
+// Utils
+#include "LX_Text.hpp"
+#include "utils/utf8_string.hpp"
+
 
 
 #endif // LUNATIX_ENGINE_H_INCLUDED
