dbc17646a184ed7a9ccf23bceec24b307e914e9a
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-09-01 19:57:39 +0200

    Refactored LX_AABB

--- include/LunatiX/LX_AABB.hpp
+++ include/LunatiX/LX_AABB.hpp
@@ -31,6 +31,26 @@ struct LX_AABB
 {
     int x, y;
     int w, h;
+
+    /// Default constructor
+    LX_AABB(): x(0), y(0), w(0), h(0) {}
+
+    /// Constructor
+    LX_AABB(const int xx, const int yy, const int ww, const int hh)
+        : x(xx), y(yy), w(ww), h(hh) {}
+
+    /// Copy constructor
+    LX_AABB(const LX_AABB& aabb): x(aabb.x), y(aabb.y), w(aabb.w), h(aabb.h) {}
+
+    /// Assignment
+    LX_AABB& operator =(const LX_AABB& aabb)
+    {
+        x = aabb.x;
+        y = aabb.y;
+        w = aabb.w;
+        h = aabb.h;
+        return *this;
+    }
 };
 
 /// Convert a constant AABB pointer to a constant SDL_Rect pointer
