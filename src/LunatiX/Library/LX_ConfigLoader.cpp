28938ac575ef0759b1a7f3b82603da99f6a994d7
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-09-22 16:22:58 +0200

    LX_ConfigLoader: Refactoring

--- src/LunatiX/Library/LX_ConfigLoader.cpp
+++ src/LunatiX/Library/LX_ConfigLoader.cpp
@@ -25,15 +25,17 @@
 #include <fstream>
 #include <regex>
 
-namespace
-{
-const char * LX_SDL_FILE="config/lxsdl.cfg";
-};
 
 namespace LX_Config
 {
 
-void loadSDLfileConfig(LX_InternalConfig& config)
+/*
+    Read the file and retrieve data in order to put it
+    into the configuration structure
+*/
+void readFile_(std::ifstream& f,LX_InternalConfig& config);
+
+void readFile_(std::ifstream& f,LX_InternalConfig& config)
 {
     const char SHARP = '#';
     const std::string ONE("1");
@@ -47,26 +49,15 @@ void loadSDLfileConfig(LX_InternalConfig& config)
     const std::regex FONT_REG("font=.+",std::regex::extended);
     const std::regex SIZE_REG("size=.+",std::regex::extended);
 
-    std::ifstream f;
-    f.open(LX_SDL_FILE,std::ios::in);
-
-    if(f.is_open() == false)
-    {
-        LX_Log::logCritical(LX_Log::LX_LOG_SYSTEM,
-                            "loadSDLfileConfig - Cannot open %s",LX_SDL_FILE);
-        return;
-    }
-
     int cpt = 0;
     std::string line;
-    std::string s;
 
     while(getline(f,line))
     {
         if(line.empty() || line[0] == SHARP)
             continue;
 
-        s = line.substr(line.find(EQUAL) + 1);
+        const std::string s = line.substr(line.find(EQUAL) + 1);
 
         switch(cpt)
         {
@@ -138,7 +129,23 @@ void loadSDLfileConfig(LX_InternalConfig& config)
             break;
         }
     }
+}
 
+void loadSDLfileConfig(LX_InternalConfig& config)
+{
+    const char * LX_SDL_FILE="config/lxsdl.cfg";
+
+    std::ifstream f;
+    f.open(LX_SDL_FILE,std::ios::in);
+
+    if(f.is_open() == false)
+    {
+        LX_Log::logCritical(LX_Log::LX_LOG_SYSTEM,
+                            "loadSDLfileConfig - Cannot open %s",LX_SDL_FILE);
+        return;
+    }
+
+    readFile_(f,config);
     f.close();
 }
 
