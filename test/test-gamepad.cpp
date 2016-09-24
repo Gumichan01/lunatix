e671021cb90fd48c8b9e171fcf3775a713e19f70
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-09-24 20:05:05 +0200

    test-gamepad: new program to get the gamepad input

---
+++ test/test-gamepad.cpp
@@ -0,0 +1,62 @@
+
+#include <LunatiX/Lunatix.hpp>
+
+using namespace LX_Device;
+
+int main(int argc, char **argv)
+{
+    LX_Init();
+    LX_Log::setDebugMode();
+
+    LX_Win::LX_WindowInfo winfo;
+    LX_Win::LX_initWindowInfo(winfo);
+    LX_Win::LX_Window w(winfo);
+
+    LX_Gamepad g;
+    g.open(0);
+
+    if(g.isConnected())
+    {
+        LX_GamepadInfo gi;
+        g.stat(gi);
+        LX_Log::log("%s",gamepadToString(gi).utf8_str());
+    }
+
+    bool d = false;
+    SDL_Event ev;
+
+    while(!d)
+    {
+        while(SDL_PollEvent(&ev))
+        {
+            switch(ev.type)
+            {
+            case SDL_KEYUP:
+                d = true;
+                break;
+
+            /// ---------------- Gamepad ----------------
+
+            case SDL_CONTROLLERBUTTONUP:
+                LX_Log::log("button → %s",stringOfButton(ev.cbutton.button).utf8_str());
+                break;
+
+            case SDL_CONTROLLERAXISMOTION:
+                if(ev.caxis.value > 8192 || ev.caxis.value < -8192)
+                {
+                    LX_Log::log("move which → %u",ev.caxis.which);
+                    LX_Log::log("move → %s",stringOfAxis(ev.caxis.axis).utf8_str());
+                }
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+
+
+    g.close();
+    LX_Quit();
+    return 0;
+}
