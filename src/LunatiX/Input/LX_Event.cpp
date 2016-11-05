a340fb0d49ad240512da5bd467fc0c550eb6c082
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-11-05 12:57:51 +0100

    LX_Event: fixed a cppcheck issue

--- src/LunatiX/Input/LX_Event.cpp
+++ src/LunatiX/Input/LX_Event.cpp
@@ -408,12 +408,12 @@ LX_EventType LX_EventHandler::getEventType() const
         break;
 
     case SDL_CONTROLLERDEVICEADDED:
-SDL_JOYDEVICEADDED:
+	case SDL_JOYDEVICEADDED:
         ty = LX_CONTROLLERDEVICEADDED;
         break;
 
     case SDL_CONTROLLERDEVICEREMOVED:
-SDL_JOYDEVICEREMOVED:
+	case SDL_JOYDEVICEREMOVED:
         ty = LX_CONTROLLERDEVICEREMOVED;
         break;
 
