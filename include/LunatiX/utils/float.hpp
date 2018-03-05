8ff884f6a03f255f235d60fc4c58b0cdac8a4772
Gumichan01 <luxon.jean.pierre@gmail.com>
2018-03-05 22:51:39 +0100

    float: library — update

--- include/LunatiX/utils/float.hpp
+++ include/LunatiX/utils/float.hpp
@@ -12,40 +12,107 @@
 #ifndef FLOAT_HPP
 #define FLOAT_HPP
 
+#include <type_traits>
+
 struct Float
 {
     float v;
     /// Unboxing ≡ static_cast<float>(Float)
-    operator float() const;
+    operator float() const noexcept;
 };
 
-/// Boxing
-inline Float fbox(float x)
+namespace FloatBox
 {
-    return Float{x};
+
+template <typename N>
+using Ftype = typename std::enable_if< std::is_arithmetic<N>::value &&
+              !std::is_same<N,float>::value >::type;
+
+/// Boxing
+template <typename N>
+inline constexpr Float fbox(Ftype<N> x) noexcept
+{
+    return Float{static_cast<float>(x)};
 }
 
-Float operator -(const Float& x);
-Float& operator ++(Float& x);
-Float operator ++(Float& x, int);
-Float& operator --(Float& x);
-Float operator --(Float& x, int);
+inline constexpr Float fbox(float x) noexcept
+{
+    return Float{ x };
+}
 
-Float operator +(const Float& x, const Float& y);
-Float operator -(const Float& x, const Float& y);
-Float operator *(const Float& x, const Float& y);
-Float operator /(const Float& x, const Float& y);
+constexpr Float FNIL = fbox(0.0f);
 
-Float& operator +=(Float& x, const Float& y);
-Float& operator -=(Float& x, const Float& y);
-Float& operator *=(Float& x, const Float& y);
-Float& operator /=(Float& x, const Float& y);
+}
 
-bool operator ==(const Float& x, const Float& y);
-bool operator !=(const Float& x, const Float& y);
-bool operator <(const Float& x, const Float& y);
-bool operator >(const Float& x, const Float& y);
-bool operator <=(const Float& x, const Float& y);
-bool operator >=(const Float& x, const Float& y);
+Float& operator ++(Float& x) noexcept;
+Float operator ++(Float& x, int) noexcept;
+Float& operator --(Float& x) noexcept;
+Float operator --(Float& x, int) noexcept;
+
+
+constexpr Float operator -(const Float& x) noexcept
+{
+    return Float{ -x.v };
+}
+
+constexpr Float operator +(const Float& x, const Float& y) noexcept
+{
+    return Float{ x.v + y.v };
+}
+
+constexpr Float operator -(const Float& x, const Float& y) noexcept
+{
+    return Float{ x.v - y.v };
+}
+
+constexpr Float operator *(const Float& x, const Float& y) noexcept
+{
+    return Float{ x.v * y.v };
+}
+
+constexpr Float operator /(const Float& x, const Float& y) noexcept
+{
+    return Float{ x.v / y.v };
+}
+
+
+Float& operator +=(Float& x, const Float& y) noexcept;
+Float& operator -=(Float& x, const Float& y) noexcept;
+Float& operator *=(Float& x, const Float& y) noexcept;
+Float& operator /=(Float& x, const Float& y) noexcept;
+
+bool operator ==(const Float& x, const Float& y) noexcept;
+bool operator !=(const Float& x, const Float& y) noexcept;
+
+
+namespace FloatMath
+{
+
+Float& abs(Float& f) noexcept;
+
+Float& acos(Float& f) noexcept;
+Float& asin(Float& f) noexcept;
+Float& atan(Float& f) noexcept;
+
+Float& cos(Float& f) noexcept;
+Float& cosh(Float& f) noexcept;
+
+Float& sin(Float& f) noexcept;
+Float& sinh(Float& f) noexcept;
+
+Float& tan(Float& f) noexcept;
+Float& tanh(Float& f) noexcept;
+
+Float& ceil(Float& f) noexcept;
+Float& floor(Float& f) noexcept;
+
+Float& exp(Float& f) noexcept;
+Float& log(Float& f) noexcept;
+Float& log10(Float& f) noexcept;
+
+Float& pow(Float& f) noexcept;
+Float& sqrt(Float& f) noexcept;
+
+}
 
 #endif // FLOAT_HPP
--- src/LunatiX/Utilities/float.cpp
+++ src/LunatiX/Utilities/float.cpp
@@ -5,81 +5,53 @@
 #include <algorithm>
 #include <limits>
 
-Float::operator float() const
+Float::operator float() const noexcept
 {
     return v;
 }
 
-Float operator -(const Float& x)
-{
-    return Float{ -x.v };
-}
-
-Float& operator ++(Float& x)
+Float& operator ++(Float& x) noexcept
 {
     x.v += 1.0f;
     return x;
 }
 
-Float operator ++(Float& x, int)
+Float operator ++(Float& x, int) noexcept
 {
-    Float tmp{ x.v++ };
-    return tmp;
+    return Float{ x.v++ };
 }
 
-Float& operator --(Float& x)
+Float& operator --(Float& x) noexcept
 {
     x.v -= 1.0f;
     return x;
 }
 
-Float operator --(Float& x, int)
+Float operator --(Float& x, int) noexcept
 {
-    Float tmp{ x.v-- };
-    return tmp;
+    return Float{ x.v-- };
 }
 
 
-Float operator +(const Float& x, const Float& y)
-{
-    return Float{ x.v + y.v };
-}
-
-Float operator -(const Float& x, const Float& y)
-{
-    return Float{ x.v - y.v };
-}
-
-Float operator *(const Float& x, const Float& y)
-{
-    return Float{ x.v * y.v };
-}
-
-Float operator /(const Float& x, const Float& y)
-{
-    return Float{ x.v / y.v };
-}
-
-
-Float& operator +=(Float& x, const Float& y)
+Float& operator +=(Float& x, const Float& y) noexcept
 {
     x.v += y.v;
     return x;
 }
 
-Float& operator -=(Float& x, const Float& y)
+Float& operator -=(Float& x, const Float& y) noexcept
 {
     x.v -= y.v;
     return x;
 }
 
-Float& operator *=(Float& x, const Float& y)
+Float& operator *=(Float& x, const Float& y) noexcept
 {
     x.v *= y.v;
     return x;
 }
 
-Float& operator /=(Float& x, const Float& y)
+Float& operator /=(Float& x, const Float& y) noexcept
 {
     x.v /= y.v;
     return x;
@@ -94,15 +66,15 @@ Float& operator /=(Float& x, const Float& y)
 *
 *   https://github.com/Nerdylicious/FloatingPointComparison
 */
-bool operator ==(const Float& x, const Float& y)
+bool operator ==(const Float& x, const Float& y) noexcept
 {
-    static const float EPSILON = std::numeric_limits<float>::epsilon();
-    static const float FLOAT_MIN = std::numeric_limits<float>::min();
-    static const float FLOAT_MAX = std::numeric_limits<float>::max();
+    static constexpr float EPSILON   = std::numeric_limits<float>::epsilon();
+    static constexpr float FLOAT_MIN = std::numeric_limits<float>::min();
+    static constexpr float FLOAT_MAX = std::numeric_limits<float>::max();
 
-    const float abs_x = fabsf(x);
-    const float abs_y = fabsf(y);
-    const float diff  = fabsf(x - y);
+    const float abs_x = std::abs(x.v);
+    const float abs_y = std::abs(y.v);
+    const float diff  = std::abs(abs_x - abs_y);
 
     if(abs_x == abs_y)
     {
@@ -118,28 +90,117 @@ bool operator ==(const Float& x, const Float& y)
     }
 }
 
-
-bool operator !=(const Float& x, const Float& y)
+bool operator !=(const Float& x, const Float& y) noexcept
 {
     return !(x == y);
 }
 
-bool operator <(const Float& x, const Float& y)
+
+namespace FloatMath
 {
-    return x.v < y.v;
+
+Float& abs(Float& f) noexcept
+{
+    f.v = std::abs(f.v);
+    return f;
 }
 
-bool operator >(const Float& x, const Float& y)
+Float& acos(Float& f) noexcept
 {
-    return x.v > y.v;
+    f.v = std::acos(f.v);
+    return f;
 }
 
-bool operator <=(const Float& x, const Float& y)
+Float& asin(Float& f) noexcept
 {
-    return !(x.v > y.v);
+    f.v = std::asin(f.v);
+    return f;
 }
 
-bool operator >=(const Float& x, const Float& y)
+Float& atan(Float& f) noexcept
 {
-    return !(x.v < y.v);
+    f.v = std::atan(f.v);
+    return f;
+}
+
+Float& ceil(Float& f) noexcept
+{
+    f.v = std::ceil(f.v);
+    return f;
+}
+
+Float& cos(Float& f) noexcept
+{
+    f.v = std::cos(f.v);
+    return f;
+}
+
+Float& cosh(Float& f) noexcept
+{
+    f.v = std::cosh(f.v);
+    return f;
+}
+
+Float& exp(Float& f) noexcept
+{
+    f.v = std::exp(f.v);
+    return f;
+}
+
+
+Float& floor(Float& f) noexcept
+{
+    f.v = std::floor(f.v);
+    return f;
+}
+
+
+Float& log(Float& f) noexcept
+{
+    f.v = std::log(f.v);
+    return f;
+}
+
+Float& log10(Float& f) noexcept
+{
+    f.v = std::log10(f.v);
+    return f;
+}
+
+Float& pow(Float& f, const Float& p) noexcept
+{
+    f.v = std::pow(f.v, p.v);
+    return f;
+}
+
+Float& sin(Float& f) noexcept
+{
+    f.v = std::sin(f.v);
+    return f;
+}
+
+Float& sinh(Float& f) noexcept
+{
+    f.v = std::sinh(f.v);
+    return f;
+}
+
+Float& sqrt(Float& f) noexcept
+{
+    f.v = std::sqrt(f.v);
+    return f;
+}
+
+Float& tan(Float& f) noexcept
+{
+    f.v = std::tan(f.v);
+    return f;
+}
+
+Float& tanh(Float& f) noexcept
+{
+    f.v = std::tanh(f.v);
+    return f;
+}
+
 }
